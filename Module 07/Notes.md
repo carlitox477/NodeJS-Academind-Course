984a888fd2374f00b7c7bd94c74b1566930c2cf8
carlitox477 <carman_boca@hotmail.com>
2021-10-17 03:53:02 -0300

    Module 07: MVC complete implementation

--- Module 07/Notes.md
+++ Module 07/Notes.md
@@ -1,2 +1,16 @@
 # Model View Control (MVC)
 ![MVC](/img/mvc.png)
+
+Model:
+* Responsible for representing your data
+* Responsible for managing your data (saving, fetching,...)
+* Doesn't matter if you manage data in memory, files, databases
+* Contanins data-related logic
+
+View:
+* What users sees
+* Shouldn't Contain too much logic
+
+Controller:
+* Connects Model and view
+* Should only make sure that the two can communicate (in both directions)
--- Module 07/models/product.js
+++ Module 07/models/product.js
@@ -1,4 +1,21 @@
-const products = []
+const fs= require('fs')
+const path = require('path')
+const currentFilePath = path.dirname(process.mainModule.filename)
+const productsJSONPath =path.join(
+    currentFilePath,
+    'data',
+    'products.json')
+
+const getProductFromFile = (cb)=>{
+    fs.readFile(productsJSONPath,(err,fileContent)=>{
+        if(err){
+            cb([])
+            return
+        }
+        cb(JSON.parse(fileContent))
+    })
+}
+
 
 module.exports = class Product{
     constructor(title){
@@ -6,11 +23,20 @@ module.exports = class Product{
     }
 
     save(){
-        products.push(this)
+        getProductFromFile(products=>{
+            products.push(this)
+            fs.writeFile(productsJSONPath, JSON.stringify(products), (err)=>{
+                if(err){
+                    console.log(err)
+                }
+                
+            })
+
+        })
     }
 
-    static fetchAll(){
-        return products
+    static fetchAll(cb){
+        getProductFromFile(cb)
     }
 
 
